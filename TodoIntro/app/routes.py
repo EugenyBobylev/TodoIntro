414cbdc5068584c23d340b04b91cc4df4441699c
Bobylev <ebobylev@gmailcom>
2020-05-26 18:11:56 +1000

    updated routes login()

--- TodoIntro/app/routes.py
+++ TodoIntro/app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -11,3 +12,9 @@ def index():
         {'descr': 'Использовать templates'}
     ]
     return render_template('index.html', user=user, tasks=tasks)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form, title='Вход')
